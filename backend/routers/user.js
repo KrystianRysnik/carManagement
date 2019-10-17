c13a48eb6de975e5084831dc49ca864ecb5dc243
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2019-10-17 19:51:58 +0200

    add server and auth

---
+++ backend/routers/user.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const User = require('../models/User')
+const auth = require('../middleware/auth')
+
+const router = express.Router()
+
+router.post('/user/register', async (req, res) => {
+    // Create new user
+    try {
+        const user = new User(req.body)
+        await user.save()
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.post('/user/login', async (req, res) => {
+    // Login a registered user
+    try {
+        const { email, password } = req.body
+        const user = await User.findCredentials(email, password)
+        if (!user) {
+            return res.status(401).send({ error: 'Logind failed! Check authentication credentials' })
+        }
+        const token = await User.generateAuthToken()
+        res.send({ user, token })
+    }
+    catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+router.get('/user/profile', auth, async (req, res) => {
+    // View logged in user profile
+    res.send(req.user)
+})
+
+router.post('/user/logout', auth, async (req, res) => {
+    // Log user out of the application
+    try {
+        req.user.tokens = res.user.tokens.filter((token) => {
+            return token.token != req.token
+        })
+        await req.user.save()
+        res.send()
+    }
+    catch (error) {
+        res.status(500).send(error)
+    }
+})
+
+module.exports = router
