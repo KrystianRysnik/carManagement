ac5826ab5f9f8dbe49b3ea2e475446ea5a8fafb8
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2019-11-05 23:40:33 +0100

    add custom drawer navigator

--- frontend/AppNavigator.js
+++ frontend/AppNavigator.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { View, Text, ImageBackground } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
@@ -12,6 +14,9 @@ import MapScreen from './screens/MapScreen';
 import CarScreen from './screens/CarScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import SettingsScreen from './screens/SettingsScreen';
+import { ScrollView } from 'react-native-gesture-handler';
+
+import store from './_store/store';
 
 
 
@@ -26,13 +31,31 @@ const AuthStack = createStackNavigator(
   }
 );
 
-const AppStack = createStackNavigator(
+const CustomDrawer = (props) => (
+  <View style={{ flex: 1 }}>
+    <View>
+    <ImageBackground source={require('./_assets/background.png')} style={{ width: '100%' }}>
+      <View style={{margin: 15, height: 50, width: 50, borderRadius: 25, backgroundColor: '#0000bb'}}>
+        <Text style={{textAlign: 'center', lineHeight: 50, color: '#ffffff', fontSize: 26, textTransform: 'uppercase'}}>{store.getState().user.currentUser.name.charAt(0)}</Text>
+      </View>
+      <Text style={{marginHorizontal: 15, color: '#ffffff'}}>{store.getState().user.currentUser.name}</Text>
+      <Text style={{marginHorizontal: 15, marginBottom: 15, color: '#ffffff', fontSize: 11}}>{store.getState().user.currentUser.email}</Text>
+      </ImageBackground>
+    </View>
+    <ScrollView>
+      <DrawerItems {...props}/>
+    </ScrollView>
+  </View>
+)
+
+const AppDrawer = createDrawerNavigator(
   {
     Profile: ProfileScreen,
     Map: MapScreen,
     Car: CarScreen
   },
   {
+    contentComponent: CustomDrawer,
     headerMode: 'none'
   }
 )
@@ -41,7 +64,7 @@ export default createAppContainer(
   createStackNavigator(
     {
       Loading: LoadingScreen,
-      App: AppStack,
+      App: AppDrawer,
       Auth: AuthStack
     },
     {
