b14a836cca8c52f0b8dc9d7bece83460ee3c695e
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2019-12-17 15:44:08 +0100

    generate pdf report

--- backend/routers/route.js
+++ backend/routers/route.js
@@ -17,19 +17,27 @@ router.post('/route/add', async (req, res) => {
 
 router.get('/route/list', async (req, res) => {
     // List of routes
-    Route.find({}, { 'markers._id' : 0 }, (err, routes) => {
+    Route.find({}, { 'markers._id': 0 }, (err, routes) => {
         if (err) {
             res.status(400).send('Something went wrong!')
             next()
         }
-        res.status(200).send(routes);
+        res.status(200).send(routes)
     });
 })
 
 router.post('/route/report', async (req, res) => {
-    // List of routes
-    console.log(req.body)
-    res.status(200).send('ok')
+    // List of routes from X (date) to Y (date) for Z (car)
+    Route.find({
+        startTrace: { $gte: new Date(req.body.dateStart), $lte: new Date(req.body.dateEnd) },
+        carVin: req.body.vin
+    }, (err, routes) => {
+        if (err) {
+            res.status(400).send('Something went wrong!')
+            next()
+        }
+        res.status(200).send(routes)
+    })
 })
 
 module.exports = router
--- frontend/screens/ReportScreen.js
+++ frontend/screens/ReportScreen.js
@@ -7,6 +7,8 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import moment from 'moment';
 import axios from 'axios';
 
+import RNHTMLtoPDF from 'react-native-html-to-pdf';
+
 class RouteScreen extends React.Component {
     static navigationOptions = {
         drawerIcon: (
@@ -22,7 +24,7 @@ class RouteScreen extends React.Component {
         dateEnd: moment().toDate(),
         mode: 'date',
         show: false,
-        car: 'all'
+        car: ''
     }
 
     setDateStart = (event, date) => {
@@ -43,12 +45,93 @@ class RouteScreen extends React.Component {
         });
     }
 
+    async createPDF(data, car) {
+        let options = {
+            html: `<style>
+            .border,
+            .border th,
+            .border td {
+                border: 1px solid black;
+                border-collapse: collapse;
+            }
+          </style>
+          <table style="width: 100%;">
+            <tr>
+                <td style="width: 50%;">(dane podatnika)</td>
+                <td style="width: 50%;">Numer rejestracyjny pojazdu: ${car.licensePlate}<br/>
+            Pojemność silnika: ${car.engineSize} cm</td>
+            </tr>
+          </table>
+
+          <h1 style="text-align: center; font-size: 16pt;">EWIDENCJA PRZEBIEGU POJAZDU DLA CELÓW VAT</h1>
+          <h3 style="text-align: center;">od ${moment(this.state.dateStart).format('DD MMM YYYY')} do ${moment(this.state.dateEnd).format('DD MMM YYYY')}</h3>`,
+            fileName: `${car.licensePlate}_${moment(this.state.dateStart).format('DD-MM-YYYY')}`,
+            directory: 'Downloads',
+            width: 595,
+            height: 842
+        };
+
+        options.html += `<table class="border">
+            <tr>
+                <th>Lp</th>
+                <th>Data wyjazdu</th>
+                <th>Cel wyjazdu</th>
+                <th>Liczba przejechanych kilometrów</th>
+                <th>Imię i nazwisko osoby kierującej</th>
+            </tr>`
+
+        let i = 1;
+        for (let item of data) {
+            options.html += `<tr>
+                <td>${i}</td>
+                <td>${moment(item.startTrace).format('DD.MM.YYYY')}</td>
+                <td>[...]</td>
+                <td>${item.distance.toFixed(2)}</td>
+                <td>${item.driver.firstName} ${item.driver.lastName}</td>
+            </tr>`
+            i++
+        }
+
+
+        options.html += `</table>`
+
+        let file = await RNHTMLtoPDF.convert(options)
+        // console.log(file.filePath);
+        alert(file.filePath);
+    }
+
+    getReport = (car) => {
+        axios.post('http://192.168.0.19:3000/route/report', {
+            dateStart: this.state.dateStart,
+            dateEnd: this.state.dateEnd,
+            licensePlate: car.licensePlate,
+            vin: car.vin
+        })
+            .then(res => {
+                console.log('🟢 File Downloaded Succesfull!')
+                this.createPDF(res.data, car)
+            })
+            .catch(error => {
+                console.log('🔴 File Downloaded Error!')
+                console.log(error)
+            })
+    }
+
     handleBack = () => {
         NavigationService.goBack()
     }
 
     handleSubmit = () => {
-        console.log('handle')
+        this.props.cars.forEach(element => {
+            if (element.vin == this.state.car) {
+                let car = {
+                    vin: element.vin,
+                    licensePlate: element.licensePlate,
+                    engineSize: element.engineSize
+                }
+                this.getReport(car)
+            }
+        });
     }
 
     render() {
@@ -56,7 +139,7 @@ class RouteScreen extends React.Component {
 
         let carsList =
             this.props.cars.map((item, index) => {
-                return (<Picker.Item label={item.name + ' - LP: ' + item.licensePlate} key={item.vin} value={item.vin} />)
+                return (<Picker.Item label={`${item.licensePlate} - ${item.name}`} key={item.vin} value={item.vin} />)
             });
 
         return (
@@ -75,13 +158,12 @@ class RouteScreen extends React.Component {
                         onValueChange={(itemValue, itemIndex) =>
                             this.setState({ car: itemValue })
                         }>
-                        <Picker.Item label='All Cars' key='all' value='all'/>
                         {carsList}
                     </Picker>
-                    <View style={{ marginVertical: 8, width: '100%', height: 1, backgroundColor: '#b6b6b6'}}></View>
+                    <View style={{ marginVertical: 8, width: '100%', height: 1, backgroundColor: '#b6b6b6' }}></View>
                     <Text>Select Start Date:</Text>
                     <TouchableOpacity onPress={() => this.setState({ showDateStart: true })}><Text style={{ padding: 8, fontSize: 16 }}>{moment(dateStart).format('Do MMM YYYY')}</Text></TouchableOpacity>
-                    <View style={{ marginVertical: 8, width: '100%', height: 1, backgroundColor: '#b6b6b6'}}></View>
+                    <View style={{ marginVertical: 8, width: '100%', height: 1, backgroundColor: '#b6b6b6' }}></View>
                     <Text>Select End Date:</Text>
                     <TouchableOpacity onPress={() => this.setState({ showDateEnd: true })}><Text style={{ padding: 8, marginBottom: 6, fontSize: 16 }}>{moment(dateEnd).format('Do MMM YYYY')}</Text></TouchableOpacity>
                     <Button title='Generate & Download PDF' onPress={this.handleSubmit} />
