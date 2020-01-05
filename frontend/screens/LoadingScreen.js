4eb6a74ed7300670382b41b2e873b28cccf17c34
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2020-01-05 19:02:55 +0100

    add asking for perrmission

--- frontend/screens/LoadingScreen.js
+++ frontend/screens/LoadingScreen.js
@@ -1,17 +1,36 @@
 import React from 'react';
-import { View, ActivityIndicator, StatusBar } from 'react-native';
+import { View, ActivityIndicator, PermissionsAndroid } from 'react-native';
 import AsyncStorage from '@react-native-community/async-storage';
 import { connect } from 'react-redux';
 import { userProfile } from '../_actions';
 
 class LoadingScreen extends React.Component {
     componentDidMount() {
-       this._bootstrapAsync();
+        this.requestForPermission()
+        this._bootstrapAsync();
     }
 
     _bootstrapAsync = async () => {
         const token = await AsyncStorage.getItem('@token');
         this.props.userProfile(token)
+    }
+
+    requestForPermission = async () => {
+        try {
+            if (Platform.OS === "android") {
+                const granted = await PermissionsAndroid.requestMultiple([
+                    PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                    PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE
+                ]);
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                    console.log('You can use the location & external storage');
+                } else {
+                    console.log('Location and/or external storage permission denied');
+                }
+            }
+        } catch (err) {
+            console.warn(err);
+        }
     }
 
     render() {
