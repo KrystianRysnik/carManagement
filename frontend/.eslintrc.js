489bfcf9f29fa809f57e36b8361f56565dda9092
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2020-08-10 16:46:39 +0200

    add eslint & prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,4 +1,29 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/react',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
+  ],
+  parser: 'babel-eslint',
+  rules: {
+    'no-use-before-define ': 0, 
+    'import/no-unresolved': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es5',
+        singleQuote: true,
+        printWidth: 100,
+      },
+    ],
+  },
+  plugins: ['prettier'],
 };
