8c86b832586d9c96460033bf294f17b52c2a514c
Krystian Ryśnik <krystian.rysnik.1@gmail.com>
2020-10-10 17:34:02 +0200

    add rules

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,11 +5,17 @@ module.exports = {
     'prettier/react',
     'plugin:prettier/recommended',
     'eslint-config-prettier',
+    'plugin:jest/recommended',
   ],
   parser: 'babel-eslint',
   rules: {
-    'no-use-before-define ': 0,
-    'import/no-unresolved': 'off',
+    'no-underscore-dangle': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/destructuring-assignment': 0,
+    'no-console': 0,
+    'react/state-in-constructor': 0,
+    'react/no-did-update-set-state': 0,
+    'no-use-before-define': ['error', {variables: false}],
     'react/jsx-filename-extension': [
       1,
       {
